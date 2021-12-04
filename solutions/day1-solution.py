5d6a24c8111a01fe8b0d449c0e715b152d932342
Matthew-Herrmann <95506901+Matthew-Herrmann@users.noreply.github.com>
2021-12-04 13:04:24 -0500

    Add files via upload

--- solutions/day1-solution.py
+++ solutions/day1-solution.py
@@ -1,25 +1,36 @@
+def get_lines(path, file):
+    """Return a list of lines in a given file from a given path"""
+    with open(path + file) as f:
+        return f.read().splitlines()
+
+
+def amount_gt_previous(nums, offset):
+    """Return amount of times that each number in a given list is greater than the previous at a given offset"""
+    amount = 0
+    for i in range(offset, len(nums)):
+        if nums[i] > nums[i - offset]:
+            amount += 1
+    return amount
+
+
+def part1(lines):
+    return amount_gt_previous(lines, 1)
+
+
+def part2(lines):
+    return amount_gt_previous(lines, 3)
+
+
 def main():
-    lines = get_input()
-    lines = list(map(lambda x: int(x), lines))
+    # Input
+    lines = get_lines("../resources/inputs/", "day1-input.txt")
+    lines = [int(x) for x in lines]
 
     # Part 1
-    count = 0
-    for i in range(1, len(lines)):
-        if lines[i] > lines[i - 1]:
-            count += 1
-    print(f"Part 1: {count}")
+    print(f"Part 1: {part1(lines)}")
 
     # Part 2
-    count = 0
-    for i in range(3, len(lines)):
-        if lines[i] > lines[i - 3]:
-            count += 1
-    print(f"Part 2: {count}")
-
-
-def get_input():
-    with open("day1-input.txt") as f:
-        return f.read().splitlines()
+    print(f"Part 2: {part2(lines)}")
 
 
 if __name__ == "__main__":
